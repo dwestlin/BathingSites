717748229c66a72092fab6a1f27f96dbff54b261
dwest <dawe1103@student.miun.se>
2018-05-21 19:25:55 +0200

    Added NewBathingSiteActivity

---
+++ app/src/main/java/se/miun/dawe1103/bathingsites/NewBathingSiteActivity.java
@@ -0,0 +1,13 @@
+package se.miun.dawe1103.bathingsites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewBathingSiteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_bathing_site);
+    }
+}
